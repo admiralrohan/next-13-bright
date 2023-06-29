ad557c0f09226d49e881e63f28f442c3cacd3d23
Rohan Gayen <rohangayen@gmail.com>
2023-06-29 17:18:49 +0000

    Update 3 files

---
+++ src/components/RevealContent/RevealContent.js
@@ -0,0 +1,16 @@
+"use client";
+import React from "react";
+
+function RevealContent({ children }) {
+  const [isRevealed, setIsRevealed] = React.useState(false);
+
+  return isRevealed ? (
+    children
+  ) : (
+    <div className="reveal">
+      <button onClick={() => setIsRevealed(true)}>Reveal Content</button>
+    </div>
+  );
+}
+
+export default RevealContent;
